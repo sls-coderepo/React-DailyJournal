cf1a07f88785c4788e276f19afb55ab74990d585
unknown <[shrestha_sl@hotmail.com]>
2019-10-05 18:07:43 -0500

    home page added

---
+++ src/Components/Journal.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Journal extends Component {
+    render(){
+        return (
+            <>
+                <h1>Test</h1>
+            </>
+        )
+    }
+}
+
+export default Journal 
+
